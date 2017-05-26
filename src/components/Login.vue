451c64f3c5e3c99220c4b0d07659e4c169c930ad
Todd Groff <tag.groff@gmail.com>
2017-05-26 03:57:46 +0100

    Login and Register forms

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,6 +5,20 @@
       <span class="tab" :class="{ active: tab == 'login' }" @click="login">Login</span>
       <span class="tab" :class="{ active : tab == 'register' }" @click="register">Register</span>
     </div>
+    <div id="register-form" v-show="tab == 'register'">
+        <div id="title">Register New Account</div>
+        <input id="uname" v-model="username" placeholder="Username"/>
+        <input type="password" id="password" v-model="password" placeholder="Password"/>
+        <input type="password" id="passconf" v-model="pass_conf" placeholder="Repeat Password"/>
+        <button id="submit" @click="submit">Register</button>
+    </div>
+    <div id="login-form" v-show="tab == 'login'">
+        <div id="title">Login</div>
+        <input id="uname" v-model="username" placeholder="Username"/>
+        <input type="password" id="password" v-model="password" placeholder="Password"/>
+        <button id="submit" @click="submit">Login</button>
+      
+    </div>
   </div>
 </div>
 </template>
@@ -14,7 +28,10 @@ module.exports = {
   data () {
     return {
       show: true,
-      tab: 'login'
+      tab: 'login',
+      username: '',
+      password: '',
+      pass_conf: ''
     }
   },
   methods: {
@@ -34,6 +51,25 @@ module.exports = {
     },
     register () {
       this.tab = 'register'
+    },
+    check_user_avail (uname) {
+
+    },
+    error () {
+      console.log('Error')
+    },
+    success () {
+      console.log('Success')
+    },
+    submit () {
+      if (this.tab === 'register') {
+        if (this.password === this.pass_conf) {
+          this.$http.post('localhost:5000/authenticate', {
+            username: this.username,
+            password: this.password
+          }).then(this.success, this.error)
+        }
+      }
     }
   }
 }
